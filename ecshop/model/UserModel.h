efc64395ac82e66db568ff1c89c6f911f3bbe2d4
TonyLin <qjwdcool@qq.com>
2016-12-16 18:08:28 +0800

    1.增加UserModel 属性

--- ecshop/model/UserModel.h
+++ ecshop/model/UserModel.h
@@ -10,4 +10,25 @@
 
 @interface UserModel : NSObject
 
+@property(nonatomic,strong)NSString *user_id;
+@property(nonatomic,strong)NSString *nick_name;
+@property(nonatomic,strong)NSString *sex;
+@property(nonatomic,strong)NSString *address;
+@property(nonatomic,strong)NSString *mobile;
+//积分
+@property(nonatomic,strong)NSString *integration;
+//关注数量
+@property(nonatomic,strong)NSString *attention;
+//钱
+@property (nonatomic,strong)NSString *user_money;
+
+@property (nonatomic,strong)NSString *birthday;
+
+@property (nonatomic,strong)NSString *email;
+//待付款数量
+@property (nonatomic,strong)NSString *pay;
+//待发货数量
+@property (nonatomic,strong)NSString *shipping_send;
+//购物车数量
+@property (nonatomic,strong)NSString *cart_num;
 @end
